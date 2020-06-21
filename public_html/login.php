509ca5b7703d8c74e3c57a86c220da1f23c2ecbd
ryotaro-boost <ryotaro.t.0727@gmail.com>
2020-06-21 23:44:21 +0900

    登録画面へのリンク

--- public_html/login.php
+++ public_html/login.php
@@ -32,6 +32,9 @@ require_once(__DIR__ . '/../config/config.php');
       <div class="btn">
         Log In
       </div>
+      <p class="fs12"><a href="/public_html/signup.php">
+        Sign Up
+      </a></p>
     </form>
   </div>
 </body>
