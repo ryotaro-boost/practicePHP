1071f0a461f2e683c3c46dc17a528d6baf142c86
ryotaro-boost <ryotaro.t.0727@gmail.com>
2020-06-21 20:15:07 +0900

    SITE_URL定数定義

--- config/config.php
+++ config/config.php
@@ -6,6 +6,8 @@ define('DSN', 'mysql:dbhost=localhost;dbuser=practice_login_php');
 define('DB_USERNAME', 'dbuser');
 define('DB_PASSWORD', 'practice365');
 
+define('SITE_URL', 'http://' . $_SERVER['HTTTP_HOST']);
+
 require_once(__DIR__ . '/../lib/functions.php');
 require_once(__DIR__ . '/autoload.php');
 
