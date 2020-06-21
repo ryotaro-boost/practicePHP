101afb972eb87261d2ea2ca04303408ddabedc72
ryotaro-boost <ryotaro.t.0727@gmail.com>
2020-06-21 20:35:55 +0900

    ログイン画面html

---
+++ public_html/login.php
@@ -0,0 +1,38 @@
+<?php
+
+//ログイン
+
+require_once(__DIR__ . '/../config/config.php');
+
+//$app = new MyApp\Controller\Login();
+//
+//$app->run();
+
+//echo 'login screen';
+//exit;
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Log In</title>
+  <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+  <div id="container">
+    <form action="" method="POST">
+      <p>
+        <input type="text" name="email" placeholder="email">
+      </p>
+      <p>
+        <input type="text" name="password" placeholder="password">
+      </p>
+      <div class="btn">
+        Log In
+      </div>
+    </form>
+  </div>
+</body>
+</html>
